4a32a33d78a0d327ac220aa8976796e155611b16
pvlm <pvl@shutoff.ru>
2016-01-16 00:48:41 +0300

    Простенький интерфейс... Просто чтоб видно было что работает.

--- lib/actions/backend/wacabBackend.action.php
+++ lib/actions/backend/wacabBackend.action.php
@@ -3,8 +3,11 @@ class wacabBackendAction extends waViewAction
 {
     public function execute()
     {
+        $model = new wacabPaymentModel();
+        $pays = $model->getAll();
         
         $message = 'Приложение Webasyst Cabinet';
         $this->view->assign('message', $message);
+        $this->view->assign('pays', $pays);
     }
 }
