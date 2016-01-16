7863c195a6010b121552122f37c40bc4b53db57c
pvlm <pvl@shutoff.ru>
2016-01-16 12:30:01 +0300

    Перенес вывод данных в раздел Transactions (бывший Parser)

--- lib/actions/backend/wacabBackend.action.php
+++ lib/actions/backend/wacabBackend.action.php
@@ -3,11 +3,8 @@ class wacabBackendAction extends waViewAction
 {
     public function execute()
     {
-        $model = new wacabPaymentModel();
-        $pays = $model->query('SELECT * FROM wacab_payment ORDER BY date DESC')->fetchAll();
-        
         $message = 'Приложение Webasyst Cabinet';
         $this->view->assign('message', $message);
-        $this->view->assign('pays', $pays);
+
     }
 }
