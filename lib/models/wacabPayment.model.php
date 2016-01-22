26c81ece8578c821a6f65224e0ad881dffed7578
Andrew Godin <covoxx@gmail.com>
2016-01-22 05:04:35 +0300

    Исправил поведение парсера если сумма больше 1000

--- lib/models/wacabPayment.model.php
+++ lib/models/wacabPayment.model.php
@@ -30,14 +30,14 @@ class wacabPaymentModel extends waModel
         }
 
         $response['recordsTotal'] = intval($this->query(
-            "SELECT count(*) FROM " . $this->table
+            "SELECT count(*) FROM " . $this->table . " WHERE pay > 0"
         )->fetchField());
 
         $searchstring = '%' . $data['search']['value'] . '%';
 
         $results = $this->query(
             "SELECT p.*, a.name AS app_name FROM " . $this->table . " p LEFT JOIN wacab_apps a ON p.app_id = a.id
-            WHERE p.order LIKE ? OR p.description LIKE ?
+            WHERE (p.order LIKE ? OR p.description LIKE ?) AND p.pay > 0
             ORDER BY " . $columns[intval($order['column'])] . " " . $this->escape($order['dir']) . "
             LIMIT " . intval($data['start']) . ", " . intval($data['length']), $searchstring, $searchstring
 
@@ -47,11 +47,11 @@ class wacabPaymentModel extends waModel
 
         $counts = $this->query(
             "SELECT COUNT(p.id) AS count, SUM(p.pay) AS sum FROM " . $this->table . " p LEFT JOIN wacab_apps a ON p.app_id = a.id
-            WHERE p.order LIKE ? OR p.description LIKE ?", $searchstring, $searchstring
+            WHERE (p.order LIKE ? OR p.description LIKE ?) AND p.pay > 0", $searchstring, $searchstring
         )->fetchAssoc();
 
         $response['recordsFiltered'] = $counts['count'];
-        $response['sum'] = $counts['sum'];
+        $response['sum'] = round($counts['sum'], 2);
 
         foreach ($results AS $i => $result) {
             $response['data'][] = array(
