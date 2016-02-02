c2ab0fa7816fd9198706d5b2eb92e798c8102710
pvlm <pvl@shutoff.ru>
2016-02-02 17:45:44 +0300

    Поменял внешний вид страницы со статистикой.

--- lib/actions/statistic/wacabStatistic.action.php
+++ lib/actions/statistic/wacabStatistic.action.php
@@ -22,6 +22,7 @@ class wacabStatisticAction extends waViewAction
             $app_return = $model->query('SELECT COUNT(*) FROM wacab_payment WHERE `pay` < 0 AND `apps_id` = '.$app['id'])->fetch();
             $names = json_decode($app['name'], true);
             $plugins_stat[] = array(
+                'id' => $app['app_id'],
                 'name' => $names[0]." (".$app['parent'].")",
                 'total' => $app_total[0],
                 'total' => $app_total[0],
