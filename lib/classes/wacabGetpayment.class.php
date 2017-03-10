5a854c0e67cc95c6b46177ad307e4f703ff24878
Pavel <pvl@shutoff.ru>
2017-03-10 11:19:36 +0300

    fix  В таблице платежей у WA порой встречаются очень странные записи. Например, номер заказа один и тот же. Поэтому переделал проверку целиком по всей записи, а не по отдельным ее атрибутам.

--- lib/classes/wacabGetpayment.class.php
+++ lib/classes/wacabGetpayment.class.php
@@ -26,12 +26,7 @@
                 unset($pays[0]);
 
                 foreach($pays as $pay){
-                    $check_pay = array(
-                        'date' => $pay['date'],
-                        'order' => $pay['order'],
-                        'description' => $pay['description']
-                    );
-                    $exist_pay = $model->getByField($check_pay);
+                    $exist_pay = $model->getByField($pay);
                     if(count($exist_pay) > 0){
                         break 2;
                     }
